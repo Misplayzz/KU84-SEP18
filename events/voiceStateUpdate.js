b3a3afe9348253bbc57b7cc8a810aa499fa60eda
Misplayzz <jaizacode@outlook.com>
2025-02-10 22:30:50 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,18 +2,25 @@ const { Events, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const config = require('../config.json');
 
+let enableConfig = {};
+try {
+    enableConfig = JSON.parse(fs.readFileSync('vctoggle.json', 'utf8'));
+} catch (error) {
+    console.error('Error reading enable.json:', error);
+}
+
 module.exports = {
     name: Events.VoiceStateUpdate,
     async execute(oldState, newState) {
-        const enableConfig = JSON.parse(fs.readFileSync('enable.json', 'utf8'));
-
         if (!enableConfig.enabled) return;
 
-        const user = newState.member.user;
+        const user = newState.member?.user;
+        if (!user) return; // ตรวจสอบว่า user มีข้อมูล
+
         const username = user.username;
         const channelId = config.vcJMLNoti;
 
-        const channel = newState.guild.channels.cache.get(channelId);
+        const channel = newState.guild?.channels.cache.get(channelId);
         if (!channel) {
             console.log('Target channel not found.');
             return;
@@ -28,17 +35,14 @@ module.exports = {
                 .setDescription(`**UserName**: ${username} has joined ${newState.channel.name}.`)
                 .setColor('#00FF00')
                 .setTimestamp();
-        }
-            else if (oldState.channelId && newState.channelId && oldState.channelId !== newState.channelId) {
+        } else if (oldState.channelId && newState.channelId && oldState.channelId !== newState.channelId) {
             // User moved from one voice channel to another
             embed = new EmbedBuilder()
                 .setTitle('User moved!')
                 .setDescription(`**UserName**: ${username} has moved from ${oldState.channel.name} to ${newState.channel.name}.`)
                 .setColor('#FFD700')
                 .setTimestamp();
-        }
-
-        else if (oldState.channelId && !newState.channelId) {
+        } else if (oldState.channelId && !newState.channelId) {
             // User left a voice channel
             embed = new EmbedBuilder()
                 .setTitle('User left!')
@@ -46,6 +50,7 @@ module.exports = {
                 .setColor('#FF0000')
                 .setTimestamp();
         }
+
         if (embed) {
             await channel.send({ embeds: [embed] });
         }
