261e75073bca451c88aedbf50fc44fdbff5cf881
Misplayzz <jaizacode@outlook.com>
2025-02-10 22:30:27 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,15 @@
 const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity(`/commandList\n [Bot version: 1.0.0]`, { type: ActivityType.Watching });
-		client.user.setStatus('ActivityType.Online');
-	}
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+        
+        // ตั้งค่า Activity (กิจกรรม)
+        client.user.setActivity('/commandList\n[Bot version: 1.0.0]', { type: ActivityType.Watching });
+
+        // ตั้งค่า status เป็น online
+        client.user.setStatus('online');
+    }
 };
